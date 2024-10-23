b2fc2db45379fe2f344e1c9a4dc8d6b1fbc2d9ba
wildREA <wildrealinit@gmail.com>
2024-10-23 13:04:50 +0200

    in-progress article

--- website/scripts.js
+++ website/scripts.js
@@ -1,7 +1,7 @@
 const headerVar = document.querySelector('#header');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 175) {
+    if (window.scrollY > 55) {
         headerVar.classList.add('scrolled');
     } else {
         headerVar.classList.remove('scrolled');
